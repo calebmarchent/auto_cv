b6b2c43073ce794c00d3940f380530a04b0f4bc4
Caleb Marchent <caleb.marchent@iee.org>
2018-03-01 22:15:35 +0000

    Now collects data from the YAML file and creates the template; so now just need to start making documents.

--- gencv.py
+++ gencv.py
@@ -3,6 +3,17 @@ CV Generator
 
 """
 from jinja2 import Environment, PackageLoader, select_autoescape
+import ruamel.yaml as yaml
+
+skills_db = {}
+
+with open("skills.yml", 'r') as stream:
+    try:
+        skills_db = yaml.load(stream, Loader=yaml.Loader)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
 env = Environment(
     loader=PackageLoader('gencv', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -10,4 +21,4 @@ env = Environment(
 
 template = env.get_template('legacy_cv.html')
 
-print template.render(name="Caleb");
+print template.render(skills_db);
