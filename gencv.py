c412758311e700f535a7018dce271090e40e9e3d
Caleb Marchent <caleb.marchent@iee.org>
2018-03-04 12:35:09 +0000

    Now all details from "Full CV" included in the document; through the formatting remains quite off

--- gencv.py
+++ gencv.py
@@ -37,16 +37,19 @@ elif OUTPUT_DOCUMENT_TYPE == "word":
 
     document.add_heading('Caleb Marchent', 0)
 
+
     p = document.add_paragraph('9 Goldfinch Drive, Cottenham, Cambridge, CB24 8XY | 07803 296105 | caleb.marchent@iee.org')
+
+    document.add_heading('Summary', level=2)
     p = document.add_paragraph('Software Engineer with 20 years experience, predominantly as a hands-on developer leading teams of highly skilled engineers to deliver products using whatever technology is required. Seeks another hands-on technical role, working closely in a team with other engineers to successfully deliver products that satisfy customers.')
     p = document.add_paragraph('Holds a rare combination of talents; a broad knowledge of the development, production and support of software, the ability to write code, script systems and use tools while at the same time - create, mentor and manage teams that can do this on a larger scale. Grace under pressure while dealing with demanding customers from around the world and delivering on tight deadlines.')
 
     document.add_heading('Key Skills', level=2)
 
     # Find the maximum number of skills in a group
-    m = max(len(skill_group) for skill_group in skills_db['skill_groups'])
+    rows = max(len(skill_group) for skill_group in skills_db['skill_groups'])
 
-    table = document.add_table(rows=m, cols=len(skills_db['skill_groups']))
+    table = document.add_table(rows=rows, cols=len(skills_db['skill_groups']))
     col = 0
     for skill_group in skills_db['skill_groups']:
         row = 0
@@ -65,6 +68,9 @@ elif OUTPUT_DOCUMENT_TYPE == "word":
         hdr_cells[1].text = position['title']
         hdr_cells[2].text = str(position['start']) + " - " + str(position['finish'])
 
+        if 'company_summary' in position:
+            document.add_paragraph(position['company_summary'])
+
         for achievement in skills_db['achievements'][position['brief']]:
             document.add_paragraph(achievement['desc'], style='ListBullet')
 
@@ -73,7 +79,7 @@ elif OUTPUT_DOCUMENT_TYPE == "word":
     for experience in skills_db['education']:
         table = document.add_table(rows=1, cols=2)
         hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = experience['desc']
+        hdr_cells[0].text = experience['desc'] + (experience['additional_info'] if 'additional_info' in experience else "")
         hdr_cells[1].text = str(experience['date'])
 
 
